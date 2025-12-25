f499821fcdf868b308d1dc60369a274d2d6813b7
Cargoland Project <cargolandproject@gmail.com>
2025-12-25 22:28:14 +0100

    its okay

--- src/common/upload/multer.ts
+++ src/common/upload/multer.ts
@@ -7,7 +7,7 @@ const uploadDir = "uploads/providers";
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
-
+// okay
 const storage = multer.diskStorage({
   destination: (_req, _file, cb) => {
     cb(null, uploadDir);
